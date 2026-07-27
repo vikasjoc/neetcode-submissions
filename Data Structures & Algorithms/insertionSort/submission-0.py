db8fc7b795924be81bbf2bf8be10dbbc9d045118
vikasjoc <vjoc3501@gmail.com>
2026-07-27 10:38:26 +0530

    Add: insertionSort - submission-0

---
+++ Data Structures & Algorithms/insertionSort/submission-0.py
@@ -0,0 +1,23 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        result = []
+
+        for i in range(len(pairs)):
+            current = pairs[i]
+            j = i - 1
+
+            while j >= 0 and pairs[j].key > current.key:
+                pairs[j + 1] = pairs[j]
+                j -= 1
+
+            pairs[j + 1] = current
+
+            # Store a copy of the current state
+            result.append(pairs[:])
+
+        return result
